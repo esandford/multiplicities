236af9777e48a378607891866f0c9ee057f10dcf
Emily Sandford <esandford@astro.columbia.edu>
2020-06-11 13:28:04 +0100

    redid missing planets calculation

---
+++ zipf/missingPlanets/results/runZipf_2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#
+#SBATCH --account=astro
+#SBATCH --job-name=runZipf_2
+#SBATCH -c 1
+#SBATCH --time=120:00:00
+#SBATCH --mem-per-cpu=3gb
+
+module load netcdf-gfortran/4.4.4
+module load netcdf-fortran/4.4.4
+
+#./make.sh
+./sim 2
+
